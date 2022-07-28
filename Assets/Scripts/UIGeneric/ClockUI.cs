3d255f62efb8eae28b7c6a058bac0165cc7cc506
auusi9 <ausi_dalmau@hotmail.com>
2022-07-28 17:34:12 +0200

    Clock and bottom UI

---
+++ Assets/Scripts/UIGeneric/ClockUI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using Workers;
+
+namespace UIGeneric
+{
+    public class ClockUI : MonoBehaviour
+    {
+        [SerializeField] private DayNight _dayNight;
+        [SerializeField] private RectTransform _rectTransform;
+
+
+        private void Update()
+        {
+            _rectTransform.localRotation = Quaternion.Euler(0, 0, _dayNight.DayProgress * -360);
+        }
+    }
+}
